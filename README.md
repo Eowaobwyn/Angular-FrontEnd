7173a9f35dd258a09f0f437cc25937e4d1c9644a
Thibaud REY <contact@thibaudrey.pro>
2018-04-14 12:00:41 +0200

    commit

--- README.md
+++ README.md
@@ -4,3 +4,17 @@
 
 # Author:
 DIEVAL Corentin
+
+Je pompe l'appli de Mr REY qui a pompé Mr PIERRAT (pour l'angular surtout, et plus spécialement le service) parce que mon appli manque une gem et que j'ai pas le courage d'aller la réinstaller.. :/
+
+Comprenez la Mr, que j'ai pas la foix.
+
+Il était une fois,
+Une marchande de foie,
+Qui vendait du foie,
+Dans la ville de Foix...
+Elle se dit ma foi,
+C'est la première fois
+Et la dernière fois,
+Que je vends du foie,
+Dans la ville de Foix
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MeteoComponent } from './meteo/meteo.component';
-import { ListeTownComponent } from './liste-town/liste-town.component'
+import { ListeTownComponent } from './liste-town/liste-town.component';
+import { AgmCoreModule } from '@agm/core';
 
 
 @NgModule({
@@ -18,7 +19,10 @@ import { ListeTownComponent } from './liste-town/liste-town.component'
   imports: [
     BrowserModule,
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCiGDkS4CdsBAYZvm9JZfUVBMRqoISNffA'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
